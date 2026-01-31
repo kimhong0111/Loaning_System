40564a9e8dbcd0a5136865e6501406142569be16
sovanpanha2007 <sovanpanha2007@gmail.com>
2026-01-31 10:03:56 +0700

    feat: add field (currentInterestsRate)

--- src/LoaningSystem.java
+++ src/LoaningSystem.java
@@ -1,15 +1,17 @@
-package src;
+package loan.src;
 
 public class LoaningSystem {
     // after making contract make sure you put the contract into the system 
     int bankId;
     String bankName;
+    double currentInterestsRate;
     Contract[] contractList;
     int count;
    
-    public LoaningSystem(String bankName,int bankId, int maxContract) {
+    public LoaningSystem(String bankName,int bankId, double currentInterestsRate, int maxContract) {
         this.bankName = bankName;
         this.bankId = bankId;
+        this.currentInterestsRate = currentInterestsRate;
         contractList = new Contract[maxContract];
         count = 0;
     }
@@ -18,8 +20,9 @@ public class LoaningSystem {
         count++;
     }
     public void displayList(){
+        System.out.print("\nList: ");
         for (int i = 0; i < count; i++) {
-            System.out.println(contractList[i].contractApplicant.name);
+            System.out.print(" "+ contractList[i].contractApplicant.name);
         }
     }
 
