e33af886d7169bbfea27d65fa98946829110e30a
sovanpanha2007 <sovanpanha2007@gmail.com>
2026-01-31 10:05:41 +0700

    feat: calculate compound interest

--- src/Contract.java
+++ src/Contract.java
@@ -1,4 +1,4 @@
-package loan.src;
+package src;
 public class Contract {
    //after CO review the application we need to make a contract make contract with applicant information amount bank name etc. contract should store application object after co review
    Applicant contractApplicant;
