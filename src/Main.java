e9cfce8d37755592283167de94200709a020988d
sovanpanha2007 <sovanpanha2007@gmail.com>
2026-01-31 10:06:32 +0700

    feat: add/change some input

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,14 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        LoaningSystem ABA = new LoaningSystem("ABA",001,10);
+        LoaningSystem ABA = new LoaningSystem("ABA",001,0.05,10);
         Applicant app1=new Applicant("Kimhong","M",2000,18);
-        Applicant app2=new Applicant("Vichea","M",100,18);
+        Applicant app2=new Applicant("Vichea","M",1000,18);
 
         Co myCo=new Co(001, 001, "ABA",10);
-        myCo.addApplication(app1,600.00,1, ABA);
-        myCo.addApplication(app2, 300.0,1, ABA);
-        ABA.addContract(null);  
+        myCo.addApplication(app1,600.00,2, ABA);
+        myCo.addApplication(app2, 300.0,1, ABA); 
         myCo.printContract();
-
+        ABA.displayList();
     }
 }
